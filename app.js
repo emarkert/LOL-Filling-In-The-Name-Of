869234092c264b0ce0530ef7c83862b5db35734a
unknown <logan@metabolicliving.com>
2022-04-11 10:54:08 -0400

    Refactor, animations

--- app.js
+++ app.js
@@ -1,53 +1,145 @@
-const topChamps = ["Aatrox", "Akali", "Akshan", "Alistar", "Camille", "Cho'Gath", "Darius", "Dr. Mundo", "Fiora", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Heimerdinger", "Illaoi", "Irelia", "Jax", "Jayce", "Karma", "Kayle", "Kennen", "Kled", "Lee Sin", "Lillia", "Lissandra", "Lucian", "Malphite", "Maokai", "Mordekaiser", "Nasus", "Olaf", "Ornn", "Pantheon", "Poppy", "Quinn", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Sett", "Shen", "Singed", "Sion", "Sylas", "Tahm Kench", "Teemo", "Trundle", "Tryndamere", "Twitch", "Urgot", "Vayne", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Zac", "Zed"]
+const DEFAULT_ROLES = []
+const DEFAULT_CHAMPS = {top: "", jungle: "", mid: "", adc: "", support: "",}
+const DEFAULT_PORTRAITS = {}
 
-const jungleChamps = ["Aatrox", "Amumu", "Darius", "Diana", "Dr. Munod", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Garen", "Gragas", "Graves", "Hecarim", "Ivern", "Jarvan IV", "Jax", "Karthus", "Kayn", "Kha'Zix", "Kindred", "Lee Sin", "Lillia", "Malphite", "Master Yi", "Mordekaiser", "Morgana", "Nasus", "Nidalee", "Nocturne", "Nunu & Willump", "Olaf", "Pantheon", "Poppy", "Rammus", "Rek'Sai", "Rengar", "Rumble", "Sejuani", "Shaco", "Shyvana", "Skarner", "Taliyah", "Talon", "Trundle", "Tryndamere", "Twitch", "Udyr", "Vi", "Viego", "Volibear", "Warwick", "Wukong", "Xin Zhao", "Zac", "Zed"]
+let champData = [];
+let selectAnimationDuration = 200; //half a second animation
+let pathsArray = []
 
-const midChamps = ["Ahri", "Akali", "Akshan", "Anivia", "Annie", "Aurelion Sol", "Azir", "Brand", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Ekko", "Ezreal", "Fizz", "Galio", "Gangplank", "Garen", "Gragas", "Graves", "Gwen", "Heimerdinger", "Illaoi", "Irelia", "Jayce", "Kai'Sa", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kog'Maw", "LeBlanc", "Lee Sin", "Lissandra", "Lucian", "Lux", "Malphite", "Malzahar", "Mordekaiser", "Morgana", "Neeko", "Orianna", "Pantheon", "Pyke", "Qiyana", "Rumble", "Ryze", "Seraphine", "Sett", "Swain", "Sylas", "Syndra", "Taliyah", "Talon", "Tristana", "Tryndamere", "Twisted Fate", "Veigar", "Vel'Koz", "Vex", "Viktor", "Vladimir", "Xerath", "Yasuo", "Yone", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"]
-
-const adcChamps = ["Akshan", "Anivia", "Aphelios", "Ashe", "Brand", "Caitlyn", "Cassiopeia", "Corki", "Draven", "Ezreal", "Graves", "Heimerdinger", "Irelia", "Jhin", "Jinx", "Kai'Sa", "Kalista", "Karthus", "Kindred", "Kog'Maw", "Lucian", "Lux", "Malzahar", "Miss Fortune", "Morgana", "Pyke", "Quinn", "Samira", "Senna", "Seraphine", "Sivir", "Swain", "Syndra", "Tahm Kench", "Teemo", "Tristana", "Twitch", "Varus", "Vayne", "Veigar", "Vel'Koz", "Xayah", "Xerath", "Yasuo", "Yone", "Zeri", "Ziggs"]
-
-const supportChamps = ["Ahri", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Bard", "Blitzcrank", "Brand", "Braum", "Cho'Gath", "Elise", "Fiddlesticks", "Galio", "Gragas", "Heimerdinger", "Ivern", "Janna", "Jarvan IV", "Kai'Sa", "Karma", "Karthus", "Kennen", "Lee Sin", "Leona", "Lissandra", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Miss Fortune", "Morgana", "Nami", "Nautilus", "Neeko", "Nidalee", "Orianna", "Pantheon", "Poppy", "Pyke", "Rakan", "Rell", "Renata Glasc", "Senna", "Seraphine", "Shaco", "Shen", "Sona", "Soraka", "Swain", "Tahm Kench", "Taliyah", "Taric", "Thresh", "Vel'Koz", "Xerath", "Yuumi", "Zilean", "Zyra"]
-
-
-let role = "";
-
-
-const champReturn = document.querySelector('.champ-select')
-const topBtn = document.querySelector('#topBtn')
-const jungleBtn = document.querySelector('#jungleBtn')
-const midBtn = document.querySelector('#midBtn')
-const adcBtn = document.querySelector('#adcBtn')
-const supportBtn = document.querySelector('#supportBtn')
+let selectedRoles = DEFAULT_ROLES
+let assignedChamps = DEFAULT_CHAMPS
+let portraitObj = DEFAULT_PORTRAITS
 
 
-topBtn.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * topChamps.length)
-    const champSelection = topChamps[randomIndex]
-    champReturn.textContent = champSelection
-})
+var request = new XMLHttpRequest();
+request.open('GET', 'champion.json', true);
 
-jungleBtn.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * jungleChamps.length)
-    const champSelection = jungleChamps[randomIndex]
-    champReturn.textContent = champSelection
-})
- 
-midBtn.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * midChamps.length)
-    const champSelection = midChamps[randomIndex]
-    champReturn.textContent = champSelection
-})
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    champData = JSON.parse(request.responseText);
 
-adcBtn.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * adcChamps.length)
-    const champSelection = adcChamps[randomIndex]
-    champReturn.textContent = champSelection
-})
+  } else {
+    // We reached our target server, but it returned an error
+    console.log('There was an error retrieving champ data from local JSON file.')
+  }
+};
 
-supportBtn.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * supportChamps.length)
-    const champSelection = supportChamps[randomIndex]
-    champReturn.textContent = champSelection
-})
+request.onerror = function() {
+  // There was a connection error of some sort
+  console.log('There was a connection error.')
+};
+
+request.send();
 
 
+addBtnTop.onclick = () => selectRole(topChamps)
+addBtnJungle.onclick = () => selectRole(jungleChamps)
+addBtnMid.onclick = () => selectRole(midChamps)
+addBtnADC.onclick = () => selectRole(adcChamps)
+addBtnSupport.onclick = () => selectRole(supportChamps)
+
+function selectRole(value) {
+    selectedRoles.push(value)
+    switch (value) {
+        case topChamps:
+            topAddImg.src = 'img/top icon.png'
+            break;
+        case jungleChamps:
+            jungleAddImg.src = 'img/jungle icon.png'
+            break;
+        case midChamps:
+            midAddImg.src = 'img/mid icon.png'
+            break;
+        case adcChamps:
+            adcAddImg.src = 'img/adc icon.png'
+            break;
+        case supportChamps:
+            supportAddImg.src = 'img/support icon.png'
+            break;
+    }
+}
+
+function diceRoll(lane) {
+    assignedChamps[lane] = getRandomChamp(lane);
+    animateSelection(lane);
+    let laneWrapper = document.querySelector('[data-role="'+lane+'"]');
+    laneWrapper.querySelector('.champ-name').innerHTML = assignedChamps[lane];
+    laneWrapper.querySelector('.champPortrait').src = `img/portraits/` + assignedChamps[lane] + `_0.jpg`;
+    laneWrapper.querySelector('.champPortrait').classList.remove('hidden')
+    laneWrapper.querySelector('.dice-icon').src = `img/reroll-button-active 1.png`;
+}
+
+function reset() {
+    selectedRoles = DEFAULT_ROLES
+    assignedChamps = DEFAULT_CHAMPS
+    assignRoles(selectedRoles);
+}
+
+const addBtns = document.querySelectorAll('.add-lane-btn')
+
+// rollBtn.onclick = () => assignRoles(selectedRoles)
+
+function updateNames() {
+    topName.innerHTML = assignedChamps.top
+    jungleName.innerHTML = assignedChamps.jungle
+    midName.innerHTML = assignedChamps.mid
+    adcName.innerHTML = assignedChamps.adc
+    supportName.innerHTML = assignedChamps.support
+}
+
+function assignRoles(roles) {
+    roles.forEach(role => getRandomChamp(role))
+    updateNames()
+    getPortraits(assignedChamps)
+}
+
+function createImgSrc(champ) {
+    let imgPath = `img/portraits/` + `${champ}` + `_0.jpg`
+    pathsArray.push(imgPath)
+}
+
+function getPortraits(obj) {
+    let portraits = (Object.values(obj))
+    portraits.forEach(champ => {
+        createImgSrc(champ)
+    });
+
+    topPortrait.src = pathsArray[0]
+    junglePortrait.src = pathsArray[1]
+    midPortrait.src = pathsArray[2]
+    adcPortrait.src = pathsArray[3]
+    supportPortrait.src = pathsArray[4]
+
+    addBtnTop.classList.add('hidden')
+    addBtnJungle.classList.add('hidden')
+    addBtnMid.classList.add('hidden')
+    addBtnADC.classList.add('hidden')
+    addBtnSupport.classList.add('hidden')
+}
+
+function getRandomChamp(lane) {
+    let champPool = champData[lane];
+    const randomIndex = Math.floor(Math.random() * champPool.length)
+    const champSelection = champPool[randomIndex]
+    return champSelection;
+}
+
+function animateSelection(lane){
+    let laneWrapper = document.querySelector('[data-role="'+lane+'"]');
+    laneWrapper.querySelector('.champ-name').classList.add('animating');
+    laneWrapper.querySelector('.champ-name').classList.remove('animated');
+    laneWrapper.querySelector('.champPortrait').classList.add('animating');
+    laneWrapper.querySelector('.champPortrait').classList.remove('animated');
+    laneWrapper.querySelector('.champPortraitWrapper').classList.add('animating');
+    laneWrapper.querySelector('.champPortraitWrapper').classList.remove('animated');
+
+    setTimeout( function(){
+        laneWrapper.querySelector('.champ-name').classList.add('animated');
+        laneWrapper.querySelector('.champ-name').classList.remove('animating');
+        laneWrapper.querySelector('.champPortrait').classList.add('animated');
+        laneWrapper.querySelector('.champPortrait').classList.remove('animating');
+        laneWrapper.querySelector('.champPortraitWrapper').classList.add('animated');
+        laneWrapper.querySelector('.champPortraitWrapper').classList.remove('animating');
+    }, selectAnimationDuration);
+}
